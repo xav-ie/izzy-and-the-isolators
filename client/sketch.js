5a657f4aafa697da7627c775e09d5994edad40cd
Andrew Elliott <andrew.elliott01@libertymutual.com>
2020-07-15 17:46:45 -0400

    Fixed color to transmit to other users. We also added a size slider for the weight of the line.

--- client/sketch.js
+++ client/sketch.js
@@ -1,6 +1,20 @@
+
+
 var socket;
+let colorPicker;
+let button;
+let weight = 10;
+let slider;
+
 
 function setup() {
+
+    createCanvas(100, 100);
+    colorPicker = createColorPicker('#ed225d');
+    colorPicker.position(0, height + 5);
+
+
+
     socket = io();
     createCanvas(windowWidth, windowHeight);
     noStroke();
@@ -16,25 +30,55 @@ function setup() {
         console.log(`server message: ${data.msg}`);
     });
 
-    socket.on("mouseMoved", ({ x, y }) => {
-        fill(255, 255, 0);
-        ellipse(x, y, 60);
+    socket.on("mouseMoved", ({ x, y, px, py, color, size}) => {
+        //fill(255, 255, 0);
+        stroke(color);
+        strokeWeight(size);
+        line(x, y, px, py);
+        //ellipse(x, y, 60);
     });
     socket.on("connect", function () {
         console.log("client connected to server");
         console.log(socket.id);
     });
+
+    button = createButton('BIG');
+    button.position(19, 19);
+    button.mousePressed(weight = weight+1);
+
+
+    slider = createSlider(1, 20, 5, 1);
+    slider.position(10, 10);
+    slider.style('width', '80px');
+
 }
 
 function draw() { }
 
+function increaseWeight() {
+    console.log("Hello");
+}
+
+
+
 function mouseDragged() {
-    console.log(`${mouseX}, ${mouseY}`);
-    fill(255);
-    ellipse(mouseX, mouseY, 60);
+    //console.log(`${mouseX}, ${mouseY}`);
+    //fill(colorPicker.color());
+    let brushSize = slider.value();
+    colors = colorPicker.value();
+
+    stroke(colorPicker.color());
+    strokeWeight(brushSize);
+    //ellipse(mouseX, mouseY, 10);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    
     const data = {
         x: mouseX,
-        y: mouseY
+        y: mouseY,
+        px: pmouseX,
+        py: pmouseY,
+        color: colors,
+        size: brushSize
     };
     socket.emit("mouseMoved", data);
 }
