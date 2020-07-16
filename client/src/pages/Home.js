e635751a61d4606459a411c22a03e4be115552ed
Tom Buckley <tbuckley00@gmail.com>
2020-07-16 14:16:03 -0500

    logo and footer adjustments

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,7 +13,7 @@ class Home extends Component {
                     <h3>Hello, and welcome.</h3>
                     <p>This website is aimed to provide users with a collaborative workspace allowing for the sharing of ideas on a digital whiteboard.</p>
                     <p>In order to create your own board room, navigate to <b>Create Board</b>. From here, select <b>Create a New Board</b>. This will then open up a new board room with a unique ID.</p>
-                    <p>This unique ID will be displayed at the top of the webpage when in your new board. If you want to invite collaborators, simply tell them your board ID, have them navigate to <b>Join Board</b> and input your boards ID.</p>
+                    <p>This unique ID will be displayed at the top of the webpage when in your new board. If you want to invite collaborators, simply tell them your board ID, have them navigate to <b>Join Board</b> and input your boards ID. Finally, click <b>Join</b> to join their board.</p>
                 </div>
             </div>
         );
