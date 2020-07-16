b01e37f7cab523bc9a0e37481df82d15c43abae7
xav-ie <xruizify@gmail.com>
2020-07-16 11:25:10 -0400

    updated reame

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./pages/styles.css";
 
 const NavBar = () => {
     return (<ul className="nav">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/createBoard">Create Board</Link></li>
-        <li><Link to="/joinBoard">Join Board</Link></li>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/createBoard">Create Board</NavLink></li>
+        <li><NavLink to="/joinBoard">Join Board</NavLink></li>
     </ul>)
-    
+
 }
 
 export default NavBar;
