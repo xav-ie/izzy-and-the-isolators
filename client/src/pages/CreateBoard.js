2ec53702364c38735624679d813e7e2391a143b7
xav-ie <xruizify@gmail.com>
2020-07-16 14:18:20 -0400

    clasName

--- client/src/pages/CreateBoard.js
+++ client/src/pages/CreateBoard.js
@@ -4,7 +4,7 @@ class CreateBoard extends Component {
     render() {
         return (
             <div className="createBoard">
-                <a href="/canvas"><button type="button" class="createNewBoard">Create a New Board</button></a>
+                <a href="/canvas"><button type="button" className="createNewBoard">Create a New Board</button></a>
             </div>
         );
     }
--- client/src/pages/JoinBoard.js
+++ client/src/pages/JoinBoard.js
@@ -4,8 +4,8 @@ class JoinBoard extends Component {
     render() {
         return (
             <div className="joinBoardContainer">
-                <form action="/" class="joinBoard">
-                    <label class="boardIDClass" for="boardID">Board ID: </label>
+                <form action="/" className="joinBoard">
+                    <label className="boardIDClass" for="boardID">Board ID: </label>
                     <input type="text" id="boardID" name="boardID"></input>
                     <input type="submit" value="Join"></input>
                 </form>
