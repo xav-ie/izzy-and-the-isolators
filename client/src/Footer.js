c54bffe632330ee2bb1e06e229cde3c23fc4c942
Tom Buckley <tbuckley00@gmail.com>
2020-07-16 14:01:36 -0500

    footer

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -3,9 +3,9 @@ import "./pages/styles.css";
 
 const Footer = () => {
     return (<footer>
-        <p>This is the footer</p>
+        <p>&copy; {new Date().getFullYear()} Izzy and the Isolators</p>
+        <p>Authors: Ennis Jackson, Izabel Ruiz, Xavier Ruiz, Tom Buckley, Jacob Walker, Andrew Elliot</p>
     </footer>);
-
 }
 
 export default Footer;
