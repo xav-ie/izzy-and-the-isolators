201977a9287db8653981796d7ff79d5de980483e
xav-ie <xruizify@gmail.com>
2020-07-16 14:49:25 -0400

    svgs!

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,9 @@ class Home extends Component {
         return (
             <div className="Home">
                 <div className="container">
+                    <div className="objectContainer">
+                        <object data="/IsolatorsLogo.svg" aria-label="Team Logo"></object>
+                    </div>
                     <h3>Hello, and welcome.</h3>
                     <p>This website is aimed to provide users with a collaborative workspace allowing for the sharing of ideas on a digital whiteboard.</p>
                     <p>In order to create your own board room, navigate to <b>Create Board</b>. From here, select <b>Create a New Board</b>. This will then open up a new board room with a unique ID.</p>
